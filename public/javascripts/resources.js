7e23158348d801f793e6aefeed4dc0d6afa4bbae
Chris McCaw <developer.cmccaw@gmail.com>
2014-02-27 21:50:06 -0800

    loading map by json instead of tmx

--- public/javascripts/resources.js
+++ public/javascripts/resources.js
@@ -1,7 +1,7 @@
 //Contains all images and level files we need
 game.resources = [
 	{name: "zombie_tileset", type:"image", src: "maps/zombie_tileset.png"},
-	{name: "area", type: "tmx", src: "maps/area.tmx"},
+	{name: "area", type: "tmx", src: "maps/startermap.json"},
 	{name: "32x32_font", type:"image", src: "images/font/32x32_font.png"},
 	{name: "alt_soldier_right", type : "image", src : "images/sprite/soldier_right_blue.png"},
 	{name: "soldier_right", type : "image", src : "images/sprite/soldier_right.png"},
